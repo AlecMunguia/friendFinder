a95fec3e4093207d1a78e67587a878d112f74b6e
Spinning Plates <alec.munguia@gmail.com>
2019-06-06 18:12:14 -0500

    submit button pushes data to the friends api

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,3 +1,35 @@
-module.exports = {
-    friendsArr : []
+let friends = [{
+    'name': "Alec",
+    'photo': "https://media.giphy.com/media/26ufq9mryvc5HI27m/giphy.gif",
+    'scores': [
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5"
+    ]
+},
+{
+    'name': "Margaret",
+    'photo': "https://media.giphy.com/media/8Oj1DDXBrevM4/giphy.gif",
+    'scores': [
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+    ]
 }
+]
+
+module.exports = friends;
--- server.js
+++ server.js
@@ -15,39 +15,3 @@ require("./app/routing/htmlRoutes")(app);
 
 // listener
 app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// //tables
-// var tables = [
-//     {
-//         friendName: "Alec",
-//         photo: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTIwNjA4NjMzNTY5NjQxOTk2/steve-buscemi-204410-1-402.jpg",
-//         scores: []
-//     }
-// ];
-
-// var friends = [];
